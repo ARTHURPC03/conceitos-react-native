d7e0db7886b1ac13b695f64afff7c7d8ca1fb399
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-04-09 19:26:04 -0300

    funcionalidades extras

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
+
+import arthur from './assets/pp.jpeg'
 
 import {
   SafeAreaView,
@@ -6,50 +9,80 @@ import {
   FlatList,
   Text,
   StatusBar,
-  StyleSheet,
+  StyleSheet, 
   TouchableOpacity,
+  Image,
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await api.get("/repositories");
+      setRepositories(response.data);
+    }
+
+    fetchData();
+  }, []);
+  
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    try {
+      const { data: newRepository } = await api.post(`/repositories/${id}/like`);
+      setRepositories(
+        repositories.map((repository) =>
+          repository.id === id ? newRepository : repository
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle="light-content" backgroundColor="#000000" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+      <View style={styles.profile}>
+        <Text style={styles.myName}>ARTHUR PC</Text>
+        
+        <Image source={arthur} style={styles.arthur} />
+      </View>
+      <FlatList
+        data={repositories}
+        keyExtractor={repository => repository.id}
+        renderItem={({ item: repository }) => (
+          <View style={styles.repositoryContainer}>
+          <Text style={styles.repository}>{repository.title}</Text>
 
           <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
+            {repository.techs.map(tech => (
+              <Text key={tech} style={styles.tech}>
+                {tech}
+              </Text>
+            ))}
+        
           </View>
 
           <View style={styles.likesContainer}>
             <Text
               style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repository.id}`}
             >
-              3 curtidas
+              {repository.likes} curtidas
             </Text>
           </View>
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            onPress={() => handleLikeRepository(repository.id)}
+            testID={`like-button-${repository.id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
         </View>
+        )}
+      />  
       </SafeAreaView>
     </>
   );
@@ -58,17 +91,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    backgroundColor: "#000000",
   },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
+    backgroundColor: "#202020",
     padding: 20,
   },
   repository: {
     fontSize: 32,
     fontWeight: "bold",
+    color: "#F5F5F5",
   },
   techsContainer: {
     flexDirection: "row",
@@ -81,7 +115,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    color: "#F5F5F5",
   },
   likesContainer: {
     marginTop: 15,
@@ -92,16 +126,35 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
+    color: "#F5F5F5",
   },
   button: {
     marginTop: 10,
   },
   buttonText: {
+    textAlign: "center",
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
-    color: "#fff",
+    color: "#F5F5F5",
     backgroundColor: "#7159c1",
     padding: 15,
   },
+  myName: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#F5F5F5",
+    textAlign: "center",
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  arthur: {
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+  },
+  profile: {
+    alignItems: "center",
+    marginBottom: 20,
+  }
 });
