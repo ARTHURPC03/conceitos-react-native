85980df67bcc912609731fbd3d43ae9b53f3cf6b
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-04-10 09:10:49 -0300

    Pequena atualizaçao

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default function App() {
               style={styles.likeText}
               testID={`repository-likes-${repository.id}`}
             >
-              {repository.likes} curtidas
+              {repository.likes} curtida{repository.likes > 1 ? 's' : ''}
             </Text>
           </View>
 
